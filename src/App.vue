e522c03695a33c97e2955d83eb5e57abfdafa635
Musta55 <bsse1114@iit.du.ac.bd>
2021-11-22 01:00:29 +0600

    reaction -timer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  
+  <Results v-if="showResults" :score='score'/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue";
+import Results from './components/Results.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.showResults=false;
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults=true;
+      this.$emit('result',this.score,this.showResults);
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +43,27 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+button {
+        border: none;
+        display: block;
+        text-align: center;
+        cursor: pointer;
+        text-transform: uppercase;
+        outline: none;
+        overflow: hidden;
+        position: relative;
+        color: #eeeeee;
+        font-weight: 600;
+        font-size: 15px;
+        background-color: #3fb1d4;
+        padding: 15px 50px;
+        margin: 10px auto;
+      }
+      button[disabled] {
+        opacity: 0.2;
+        cursor:not-allowed;
+      }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+      console.log(this.delay);
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit('end',this.reactionTime);
+    },
+  },
+  //   updated() {
+  //       console.log("component updated");
+  //   },
+  //   unmounted() {
+  //       console.log("component unmounted");
+  //   }
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: crimson;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
